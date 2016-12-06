dd5bf4cf1a9f15892c28901d9c76f78967948c8a
gsa390 <mail@giovannidecostanzo.it>
2016-12-06 12:24:30 +0100

    readBMP

--- src/scan/seeBPMImage.java
+++ src/scan/seeBPMImage.java
@@ -13,18 +13,31 @@ public class seeBPMImage {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedImage image = ImageIO.read(new File("lena_gray.bmp"));
-        //BufferedImage image = ImageIO.read(new File("lena512.bmp"));
+        //BufferedImage image = ImageIO.read(new File("lena_gray.bmp"));
+        BufferedImage image = ImageIO.read(new File("lena512.bmp"));
         
         int[][] array2D = new int[image.getWidth()][image.getHeight()];
-        for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
-            for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {      
+        int x = 0,y = 0;
+        for (int yPixel = 0; yPixel < image.getHeight(); yPixel++, x++){
+        	y=0;
+        	for (int xPixel = 0; xPixel < image.getWidth(); xPixel++, y++) {      
 
                     int color = image.getRGB(xPixel, yPixel);
                     System.out.println("Pixel [" + xPixel + "," + yPixel + "]: " + (color & 0xFF) );	
-                    array2D[xPixel][yPixel] = color & 0xFF;
+                    array2D[x][y] = color & 0xFF;
                     
             }
         }
+        
+        BufferedImage image2;
+        for(int i=0; i<array2D.length; i++) {
+            for(int j=0; j<array2D.length; j++) {
+                int a = array2D[i][j];
+                Color newColor = new Color(a,a,a);
+                image.setRGB(j,i,newColor.getRGB());
+            }
+        }
+        File output = new File("GrayScale.jpg");
+        ImageIO.write(image, "jpg", output);
     }
 }
