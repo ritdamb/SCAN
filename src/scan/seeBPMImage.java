4abd2876b1e608b0f9c254b1b82a6186f2e68ed4
gsa390 <mail@giovannidecostanzo.it>
2016-12-06 11:41:58 +0100

    seeBMPImage

--- src/scan/seeBPMImage.java
+++ src/scan/seeBPMImage.java
@@ -13,36 +13,18 @@ public class seeBPMImage {
 
     public static void main(String[] args) throws IOException {
 
-        //BufferedImage image = ImageIO.read(new File("/Users/giovanni/lena_gray.bmp"));
-        BufferedImage image = ImageIO.read(new File("lena512.bmp"));
+        BufferedImage image = ImageIO.read(new File("lena_gray.bmp"));
+        //BufferedImage image = ImageIO.read(new File("lena512.bmp"));
+        
         int[][] array2D = new int[image.getWidth()][image.getHeight()];
-
-        //byte[] pixels = extractBytes("/Users/giovanni/lena_gray.bmp");
-        //byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-        //System.out.println(pixels.length);
-        //for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
-        for (int xPixel = 0; xPixel < 100; xPixel++) {
-            //for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {
-            for (int yPixel = 0; yPixel < 100; yPixel++) {
+        for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
+            for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {      
 
                     int color = image.getRGB(xPixel, yPixel);
-                    System.out.println("Pixel [" + xPixel + "," + yPixel + "]: " + (color & 0xFF) );
-                    if (color == Color.BLACK.getRGB()) {
-                        array2D[xPixel][yPixel] = 1;
-                    } else array2D[xPixel][yPixel] = 0; // ?
+                    System.out.println("Pixel [" + xPixel + "," + yPixel + "]: " + (color & 0xFF) );	
+                    array2D[xPixel][yPixel] = color & 0xFF;
+                    
             }
         }
-    }
-
-    public static byte[] extractBytes (String ImageName) throws IOException {
-        // open image
-        File imgPath = new File(ImageName);
-        BufferedImage bufferedImage = ImageIO.read(imgPath);
-
-        // get DataBufferBytes from Raster
-        WritableRaster raster = bufferedImage .getRaster();
-        DataBufferByte data   = (DataBufferByte) raster.getDataBuffer();
-
-        return ( data.getData() );
     }
 }
