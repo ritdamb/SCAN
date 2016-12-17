e22a890c44a9c7a50d95aabe4513448e7559f5d7
Rita <rita.dambrosio@gmail.com>
2016-12-17 16:30:03 +0100

    i/o package per la scrittura di bit

---
+++ src/io/WriteBit.java
@@ -0,0 +1,38 @@
+package io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+public class WriteBit {
+
+	public static void main(String[] args){
+		
+		String header1="111";
+		String scanpath="100110111000001001010110001111010000000000101";
+		FileOutputStream file=null;
+		
+		try {
+			file = new FileOutputStream("compress");
+			BitOutputStream out = new BitOutputStream(file);
+			
+			String toWrite = header1+scanpath;
+			System.out.println("bit da scrivere : " + toWrite.length());
+			for(int i=0; i < toWrite.length(); i++)
+				if(toWrite.charAt(i) == '1')
+					out.write(1);
+				else
+					out.write(0);
+			
+			out.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
